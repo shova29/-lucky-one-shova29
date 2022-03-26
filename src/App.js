ed5ef1114ef93282a6771885bf9c0e3142832db8
shova29 <halima2996@gmail.com>
2022-03-26 23:01:16 +0600

    qna added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Shop from "./components/Shop/Shop";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
+import QnA from "./components/QnA/QnA";
 
 function App() {
   return (
     <div className="app-body">
       <Header></Header>
       <Shop></Shop>
+      <QnA></QnA>
     </div>
   );
 }
--- src/components/QnA/QnA.js
+++ src/components/QnA/QnA.js
@@ -1,8 +1,66 @@
 import React from "react";
+import { Accordion } from "react-bootstrap";
 import "./QnA.css";
 
 const QnA = () => {
-  return <div></div>;
+  return (
+    <div className="mt-5 mb-5">
+      <Accordion defaultActiveKey={["0"]} alwaysOpen>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>#1 How does react works?</Accordion.Header>
+          <Accordion.Body>
+            <p>
+              React makes it easier to create dynamic web applications because
+              it requires less coding and offers more functionality, as opposed
+              to JavaScript, where coding often gets complex very quickly. React
+              uses Virtual DOM, thereby creating web applications faster.
+              Virtual DOM compares the components’ previous states and updates
+              only the items in the Real DOM that were changed, instead of
+              updating all of the components again, as conventional web
+              applications do. Components are the building blocks of any React
+              application, and a single app usually consists of multiple
+              components. These components have their logic and controls, and
+              they can be reused throughout the application, which in turn
+              dramatically reduces the application’s development time. React
+              follows a unidirectional data flow. This means that when designing
+              a React app, developers often nest child components within parent
+              components.
+            </p>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            #2 What is the difference between Props vs State?
+          </Accordion.Header>
+          <Accordion.Body>
+            <p>
+              1. Props permits to pass data from one component to different
+              components as an argument. But state holds information concerning
+              the components that has to be bestowed to that store the read
+              within the state.
+              <p>
+                {" "}
+                2. Props are immutable so they cannot be changed or modified
+                when they have been set. But the data is keep within the state,
+                which could be modified over time.
+              </p>{" "}
+              <p>
+                {" "}
+                3. Props are often accessed with its own child component. Where,
+                the state cannot be accessed through its child components.
+              </p>{" "}
+              <p>
+                {" "}
+                4. Props are accustomed to communicate between components.
+                Whereas, states can be used for rendering the dynamic changes
+                within the component.
+              </p>
+            </p>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
 };
 
 export default QnA;
